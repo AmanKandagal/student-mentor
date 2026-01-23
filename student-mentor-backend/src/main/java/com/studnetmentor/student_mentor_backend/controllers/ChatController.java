2af2adb763be415498ce84e39c44a3e564031e30
Aman8427 <amankandagal18@gmail.com>
2026-01-23 16:56:36 +0530

    docs: add comment explaining mentor-student logic

--- student-mentor-backend/src/main/java/com/studnetmentor/student_mentor_backend/controllers/ChatController.java
+++ student-mentor-backend/src/main/java/com/studnetmentor/student_mentor_backend/controllers/ChatController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+//HI
 @RestController
 @RequestMapping("/api/chat")
 @CrossOrigin(origins = "http://localhost:4200") // Allow Angular to access
